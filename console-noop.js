5c22c56cc6eacf2a007f1f8ca3ae31015365b3b1
Wesley Anemam <wesleylira@gmail.com>
2015-09-21 02:23:48 +0100

    Release v1.0.0

--- console-noop.js
+++ console-noop.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports = function(window, console){
 	if (console || (window && window.console)) return
 
@@ -29,3 +30,8 @@ module.exports = function(window, console){
 	for (var index = 0, length = methods.length; index < length; index++)
 		window.console[methods[index]] = noop
 }
+},{}],2:[function(require,module,exports){
+(function (window, console) {
+	require('../console-noop.js')(window, console);
+})(window, window.console);
+},{"../console-noop.js":1}]},{},[2])
